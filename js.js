d9934729298ad61cfad49e8dcfa3fbc34f4fbcfd
LiveWaffle <livewaffle@proton.me>
2024-08-02 21:09:36 -0400

    broken fade got to fix

--- js.js
+++ js.js
@@ -8,3 +8,17 @@ function DarkMode() {
     document.getElementById("Light").innerHTML = "☀️"; 
   }
 }
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) =>{
+    console.log(entry)
+      if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+      } else {
+          entry.target.classList.remove('show');
+      }
+  });
+});
+
+const hidden = document.querySelectorAll('.hidden')
+hidden.forEach((el) =>observer.observe(el));
